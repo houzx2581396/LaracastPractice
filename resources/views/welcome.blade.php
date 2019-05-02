037167745d52f89d0ee17db4cc27e73f5f61a952
Nick <zx2581396@gmail.com>
2019-05-02 10:11:22 +0800

    New home view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,10 @@
 
 
 @section('content')
-    <h1>My Website</h1>
+    <h1>My {!! $foo !!}Website</h1>
+    <ul>
+        @foreach($tasks as $task)
+            <li>{{ $task }}</li>
+        @endforeach
+    </ul>
 @endsection
