c2990546d3146e423429c6bdcc39baf8e1ab1e19
Nick <zx2581396@gmail.com>
2019-05-11 16:25:21 +0800

    Modify ProjectsController

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -7,8 +7,25 @@ class ProjectsController extends Controller
 {
     public function index()
     {
-        $projects = \App\Project::all();
-        return Project::all();
+        $projects = Project::all();
+
         return view('projects.index',compact('projects'));
     }
+
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    public function store()
+    {
+        $project = new Project();
+
+        $project->title = request('title');
+        $project->description = request('description');
+        $project->save();
+
+        return redirect('/projects');
+    }
+
 }
