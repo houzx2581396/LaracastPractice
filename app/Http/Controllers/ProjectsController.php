352e21eec785b93666df99a034d21f27b62fffec
Nick <zx2581396@gmail.com>
2019-05-13 15:08:00 +0800

    Update project

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -17,9 +17,14 @@ class ProjectsController extends Controller
         return view('projects.create');
     }
 
-    public function update()
+    public function update($id)
     {
+        $project = Project::find($id);
+        $project->title = request('title');
+        $project->description = request('description');
+        $project->save();
 
+        return redirect('/projects');
     }
 
     public function show()
@@ -27,9 +32,10 @@ class ProjectsController extends Controller
 
     }
 
-    public function edit()
+    public function edit($id)
     {
-
+        $project = Project::find($id);
+        return view('projects.edit',compact('project'));
     }
 
     public function destroy()
