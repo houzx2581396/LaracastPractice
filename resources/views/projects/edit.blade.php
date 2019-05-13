06f09d2bc8f4292ff0b1057f0f6ad263c430c73c
Nick <zx2581396@gmail.com>
2019-05-13 15:09:11 +0800

    New editview

---
+++ resources/views/projects/edit.blade.php
@@ -0,0 +1,32 @@
+@extends('layout')
+
+@section('content')
+    <h1 class="title">Edit project</h1>
+
+    <form method="post" action="/projects/{{$project->id}}">
+        {{method_field('PATCH')}}
+        {{csrf_field()}}
+
+        <div class="field">
+            <label class="label" for="title">title</label>
+
+            <div class="control">
+            <input type="text" class="input" name="title" placeholder="Title" value="{{$project->title}}">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label" for="description">Description</label>
+
+            <div class="control">
+                <textarea class="textarea" name="description">{{$project->description}}</textarea>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">Update project</button>
+            </div>
+        </div>
+    </form>
+@endsection
