54859fff71e34b0a20ea2bc0037c58025dc446ef
Nick <zx2581396@gmail.com>
2019-05-13 13:42:22 +0800

    Add Route

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,25 @@ Route::get('/',function (){
     return view('welcome');
 });
 Route::get('/','PagesController@home');
-
 Route::get('/about','PagesController@about');
-
 Route::get('/contact','PagesController@contact');
 
-Route::get('/projects','ProjectsController@index');
-Route::post('/projects','ProjectsController@store');
-Route::get('/projects/create','ProjectsController@create');
+/*
+ * Get /projects (index)
+ * post /projects (store)
+ * Get /projects/create (create)
+ * Get /projects/1 (show)
+ * PATCH /projects/1 (update)
+ * DELETE /projects/1 (destroy)
+ * Get /projects/edit (edit)
+ *
+ */
+
+Route::resource('/projects','ProjectsController');
+//Route::get('/projects','ProjectsController@index');
+//Route::post('/projects','ProjectsController@store');
+//Route::get('/projects/create','ProjectsController@create');
+//Route::get('/projects/{projects}/edit','ProjectsController@edit');
+//Route::patch('/projects/{projects}','ProjectsController@update');
+//Route::get('/projects/{projects}','ProjectsController@show');
+//Route::delete('/projects/{projects}','ProjectsController@destroy');
