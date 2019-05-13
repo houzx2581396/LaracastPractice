aeba7abe03fa65d09df9641fdebd4421593c25e4
Nick <zx2581396@gmail.com>
2019-05-13 13:41:23 +0800

    Modify ProjectsController

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -17,6 +17,26 @@ class ProjectsController extends Controller
         return view('projects.create');
     }
 
+    public function update()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
     public function store()
     {
         $project = new Project();
