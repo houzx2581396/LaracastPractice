b4d9d9fc089ed75fac73e94b17c0422f2e4e9ac1
Nick <zx2581396@gmail.com>
2019-05-16 15:42:18 +0800

    Modify Route

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/contact', 'PagesController@contact');
 Route::resource('/projects', 'ProjectsController');
 
 Route::post('/projects/{project}/tasks', 'ProjectTasksController@store');
-Route::patch('/tasks/{task}', 'ProjectTasksController@update');
+Route::post('complete-tasks/{task}', 'CompletedController@store');
+Route::delete('complete-tasks/{task}', 'CompletedController@destroy');
